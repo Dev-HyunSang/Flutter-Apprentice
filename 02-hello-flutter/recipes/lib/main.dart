0a749f1163a26cad693535e6277083f02ac2640e
HyunSang Park <me@hyunsang.dev>
2021-11-21 21:05:54 +0900

    Feat: Add Design at Padding and Add Recipe detail Page

--- 02-hello-flutter/recipes/lib/main.dart
+++ 02-hello-flutter/recipes/lib/main.dart
@@ -43,20 +43,44 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index) {
-            return buildRepcipeCard(Recipe.samples[index]);
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return Text('Detail Page');
+                  }),
+                );
+              },
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
           },
         ),
       ),
     );
   }
 
-  Widget buildRepcipeCard(Recipe recipe) {
+  Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      child: Column(
-        children: <Widget>[
-          Image(image: AssetImage(recipe.imageUrl)),
-          Text(recipe.label),
-        ],
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Palatino'),
+            )
+          ],
+        ),
       ),
     );
   }
