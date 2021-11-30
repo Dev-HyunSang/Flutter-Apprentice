253630c390bf8a3a8c14f78f8efe9d43ffa2527b
HyunSang Park <me@hyunsang.dev>
2021-11-30 21:26:01 +0900

    Feat: Adding ListView for today’s recipes

--- 05-scrollable-widgets/starter/lib/components/today_recipe_list.dart
+++ 05-scrollable-widgets/starter/lib/components/today_recipe_list.dart
@@ -27,11 +27,33 @@ class TodayRecipeListView extends StatelessWidget {
           const SizedBox(height: 16),
           Container(
             height: 400,
-            color: Colors.grey,
+            color: Colors.transparent,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: recipes.length,
+              itemBuilder: (context, index) {
+                final recipe = recipes[index];
+                return buildCard(recipe);
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 16);
+              },
+            ),
           )
         ],
       ),
     );
   }
-  // TODO: Add buildCard() widget here
+
+  Widget buildCard(ExploreRecipe recipe) {
+    if (recipe.cardType == RecipeCardType.card1) {
+      return Card1(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card2) {
+      return Card2(recipe: recipe);
+    } else if (recipe.cardType == RecipeCardType.card3) {
+      return Card3(recipe: recipe);
+    } else {
+      throw Exception('This card doesn\'t exist yet');
+    }
+  }
 }
